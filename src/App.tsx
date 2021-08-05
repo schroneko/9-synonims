2df4d14f9e15028212f33ce6d2e062fc003e33b6
schroneko <hash@MyMBA.local>
2021-08-06 00:55:58 +0900

    fix: refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import InputBox from "./InputBox";
+import "./index.css";
 
-function App() {
+export const App: React.VFC = () => {
+  const items = [];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      items.push(<InputBox key={3 * i + j} />);
+    }
+    items.push(<br key={9 + i} />);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="boxes">{items}</div>
+      <hr />
+      <div className="text">
+        <h1>9マス類語変換ゲーム</h1>
+        <h2>ルールは簡単！</h2>
+        <h3>1. まんなかに一つ言葉を書いてね</h3>
+        <h3>2. 周りに思いつく類語を書いてね</h3>
+        <h3>3. 全てのマスを埋めたら完成だよ</h3>
+      </div>
+    </React.Fragment>
   );
-}
-
-export default App;
+};
---
+++ src/InputBox.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+const InputBox: React.VFC = () => {
+  return (
+    <React.Fragment>
+      <p contentEditable className="box">
+        {/* Content */}
+      </p>
+    </React.Fragment>
+  );
+};
+
+export default InputBox;
